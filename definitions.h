ac808ee0ca1181b94d9865df0611f0eaddcd3a52
Phil Canete <philcanete1@gmail.com>
2019-03-17 23:52:30 -0700

    initial push

---
+++ definitions.h
@@ -0,0 +1,19 @@
+//Anemometer Technical Variables
+//The following variables correspond to the anemometer sold by Adafruit, but could be modified to fit other anemometers.
+
+const int sensorPin = A0; //Defines the pin that the anemometer output is connected to
+
+float voltageConversionConstant = .004882814; //This constant maps the value provided from the analog read function, which ranges from 0 to 1023, to actual voltage, which ranges from 0V to 5V
+int sensorDelay = 1000; //Delay between sensor readings, measured in milliseconds (ms)
+
+float voltageMin = .4; // Mininum output voltage from anemometer in mV.
+float windSpeedMin = 0; // Wind speed in meters/sec corresponding to minimum voltage
+
+float voltageMax = 2.0; // Maximum output voltage from anemometer in mV.
+float windSpeedMax = 32; // Wind speed in meters/sec corresponding to maximum voltage
+
+//Temp and Humidity sensor variables
+// Connect RED of the AM2315 sensor to 5.0V
+// Connect BLACK to Ground
+// Connect WHITE to i2c clock - on '168/'328 Arduino Uno/Duemilanove/etc thats Analog 5
+// Connect YELLOW to i2c data - on '168/'328 Arduino Uno/Duemilanove/etc thats Analog 4
---
+++ timedEWBsensors.ino
@@ -0,0 +1,67 @@
+#include <TimerOne.h>
+#include <Wire.h>
+#include <Adafruit_AM2315.h>
+#include "definitions.h"
+
+
+volatile int secondsTimer;  //Incremented by ISR
+int seconds = 0;            //Initialize non volatile seconds timer
+bool clearTimer = false;    //Signals to clear timer every ### seconds
+Adafruit_AM2315 am2315;
+
+void setup() {
+  Timer1.initialize(1000000);   //Initialize timer to interrupt at 1 second per period
+  Timer1.attachInterrupt(isr);  //Go to interrupt service routine every period
+  am2315.begin();                //Initialize adafruit am2315 library
+  Serial.begin(9600);
+}
+
+void loop() {  
+  noInterrupts();    //Must turn off interrupts while accessing volatile variables
+    seconds = secondsTimer;   //Set non voltile seconds timers
+    if(clearTimer)  //This loop clears the timer and the timer signal when set in the loop
+      {
+        secondsTimer = 0;
+        Timer1.restart();
+        clearTimer = false;
+      } 
+  interrupts();      //Turn on interrupts after accessing variable
+
+  
+ if(seconds == 5) {  //Change this to change seconds between data logs
+        Serial.print("Windspeed: "); Serial.println(windspeed());
+        Serial.print("Humidity: "); Serial.println(am2315.readHumidity());
+        Serial.print("Temperature: "); Serial.println(am2315.readTemperature());
+        
+        //insert data logging here
+        clearTimer = true;
+        seconds = 0;
+    }
+}
+
+void isr() {
+  secondsTimer++;
+}
+
+int windspeed() {
+  int sensorValue = 0; //Variable stores the value direct from the analog pin
+  float sensorVoltage = 0; //Variable that stores the voltage (in Volts) from the anemometer being sent to the analog pin
+  float windSpeed = 0; // Wind speed in meters per second (m/s)
+  
+  sensorValue = analogRead(sensorPin); //Get a value between 0 and 1023 from the analog pin connected to the anemometer
+  sensorVoltage = sensorValue * voltageConversionConstant; //Convert sensor value to actual voltage
+  
+  //Convert voltage value to wind speed using range of max and min voltages and wind speed for the anemometer
+  if (sensorVoltage == voltageMin){
+   windSpeed = 0; //Check if voltage is below minimum value. If so, set wind speed to zero.
+  }else {
+    windSpeed = (sensorVoltage - voltageMin)*windSpeedMax/(voltageMax - voltageMin); //For voltages above minimum value, use the linear relationship to calculate wind speed.
+  }
+}
+
+//todo
+//1. add temp+humidity code
+//2. add wind speed code
+//3. add fona808 code
+//4. make time between data logs a variable
+//5. turn insantaneous sensing into average
